12262207d527ab70aedc309f61a2b45bcb79f745
Juan M Hernandez <juan_hernandez@engineering.ucla.edu>
2018-05-05 20:25:06 -0700

    Skeleton MVC Spring Boot Application w/ sample UI

---
+++ spring18/src/main/java/com/sylconnexity/spring18/Controllers.java
@@ -0,0 +1,34 @@
+package com.sylconnexity.spring18;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+class index_Controller {
+
+    @GetMapping("/")
+    public ModelAndView example(@RequestParam(value = "name", defaultValue="User") String value){
+        ModelAndView result = new ModelAndView();
+        result.addObject("userName", value);
+
+        result.setViewName("index");
+
+        return result;
+    }
+
+}
+
+// Controller for a sample of an analytics UI
+@Controller
+class sampleUI_Controller {
+    @GetMapping("/sampleUI")
+    public ModelAndView sample_view(){
+        ModelAndView result = new ModelAndView();
+        result.setViewName("sampleAnalyticsUI/sample");
+        return result;
+    }
+
+
+}
