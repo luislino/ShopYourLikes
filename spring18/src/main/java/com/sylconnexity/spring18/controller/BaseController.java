d7969a144cbab721f93a40d25c03d70f153a66ce
Ronald Gao <ronald.s.gao@gmail.com>
2018-05-24 18:16:49 -0700

    Made Controller -> BaseController and fixed dependencies

--- spring18/src/main/java/com/sylconnexity/spring18/controller/BaseController.java
+++ spring18/src/main/java/com/sylconnexity/spring18/controller/BaseController.java
@@ -4,16 +4,19 @@ import com.sylconnexity.spring18.dbschema.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
 
 /**
- * Controller for the Link class, the API of which is available at /links.
+ * Base Controller
  */
 @Controller
-@RequestMapping(path="/links")
-public class DBController_Link {
+@RequestMapping(path="")
+public class BaseController {
     @Autowired
     private ClickRepository clickRepository;
     @Autowired
